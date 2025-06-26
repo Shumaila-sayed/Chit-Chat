2e0383c834447407bb92e010e90d192ae3b99ec3
Shumaila <sayedshumaila27@gmail.com>
2025-06-27 00:45:30 +0530

    Setup index template

--- app.js
+++ app.js
@@ -8,6 +8,23 @@ const path = require("node:path");
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+const messages = [
+	{
+		text: 'Hi there!',
+		user: 'Amando',
+		added: new Date(),
+	},
+	{
+		text: 'Hello World!',
+		user: 'Charles',
+		added: new Date(),
+	},
+];
+
+app.get('/', (req, res) => {
+	res.render('index', {title: "Chit Chat", messages: messages});
+});
+
 // for routes
 app.use("/new", newMessageRouter);
 app.use("/", indexRouter);
